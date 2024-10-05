44b28c95f69305bc56b2e5eb150af5b9cd1d4d56
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-10-05 17:18:32 +0200

    add reverse proxy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        ws: true,
-      },
-    }
-  }
 })
